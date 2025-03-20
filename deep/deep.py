b4d38af11b21032619b9fc239f3ce684aa109274
khemPaneru <khempaneru2@gmail.com>
2025-03-20 00:49:06 +0000

    Wed, Mar 19, 2025, 8:49 PM -04:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,1 +1,5 @@
-input("What is the answer to the Great Question of Life, the Universe and Everything?").
+
+def main():
+    user_input = input("What is the answer to the Great Question of Life, the Universe and Everything?").strips().lowercase()
+        if user_input == 42 or user_input == forty-two or forty two== forty two
+
