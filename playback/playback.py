cb26f8e0e20f31972581ce3ad557c4626acd53dd
khemPaneru <khempaneru2@gmail.com>
2025-03-20 02:03:36 +0000

    Wed, Mar 19, 2025, 10:03 PM -04:00

--- playback/playback.py
+++ playback/playback.py
@@ -1,6 +1,6 @@
 def main():
-    user_input = input("Enter the messages you want to say: ").replace("...")
+    user_input = input("Enter the messages you want to say: ").replace(" " ,"...")
 
-    print(f"playspeed: {user_input})
+    print(f"playspeed: {user_input}")
 
 main()
