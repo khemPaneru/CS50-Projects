5930ccf39b0bb93c384ffa1c507c7bba7ee9fe97
khemPaneru <khempaneru2@gmail.com>
2025-03-20 01:55:52 +0000

    Wed, Mar 19, 2025, 9:55 PM -04:00

--- playback/playback.py
+++ playback/playback.py
@@ -1,4 +1,4 @@
 def main():
-    user_input = input("Enter the messages you wants to say" ?)
+    user_input = input("Enter the messages you wants to say" )
 
-        print(f"{user_input} z:, ")
+        print(f"{user_input}z:,")
