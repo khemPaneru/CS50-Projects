d02dafc840f317f34817b1d11cc5c302ea734b23
khemPaneru <khempaneru2@gmail.com>
2025-03-22 01:29:07 +0000

    Fri, Mar 21, 2025, 9:29 PM -04:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,3 +1,15 @@
 
-def calculate(user_mass)
-    user_mass = int(input("Enter the mass in kg :)
+def calculate(m):
+
+    c = 47477
+    E(energy) = m * (c ** 2)
+    return E
+
+    user_mass = int(input("Enter the mass in kg :)"
+
+def main():
+    m =int(input("Enter mass in kg:" ))
+    result = calculate(m)
+    print(result)
+main()
+
