3e6be730dc198e15e30563232134cdc84604d246
khemPaneru <khempaneru2@gmail.com>
2025-03-22 00:55:58 +0000

    Fri, Mar 21, 2025, 8:55 PM -04:00

--- faces/faces.py
+++ faces/faces.py
@@ -2,9 +2,10 @@
 def convert():
    khem =input_in_string.replace(":) " ,"🙂").replace(":( " ,"🙁")
     return khem
+
 def main():
-   user_input= input("Give your input in emoticons")
+    user_input= input("Give your input in emoticons")
     result = convert(user_input)
     print(result)
-
+    
 main()
