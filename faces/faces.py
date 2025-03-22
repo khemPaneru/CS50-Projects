b92bc6016dd67c874e694df31b60c5d10782d780
khemPaneru <khempaneru2@gmail.com>
2025-03-22 01:05:40 +0000

    Fri, Mar 21, 2025, 9:05 PM -04:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,10 +1,15 @@
 
+
+#made a function
 def convert(string_input):
+    #use_replaec_method
     khem = string_input.replace(":)" ,"🙂").replace(":(" ,"🙁")
     return khem
 
 def main():
     user_input= input("Give your input in emoticons")
+    #call the function with arug
+    
     result = convert(user_input)
     print(result)
 
