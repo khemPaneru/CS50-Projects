7a78f3a877b1275b91d243eb7170d1433d50b8b5
khemPaneru <khempaneru2@gmail.com>
2025-03-23 17:44:07 +0000

    Sun, Mar 23, 2025, 1:44 PM -04:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,3 +1,5 @@
+
+
 def main():
     dollars = dollars_to_float(input("How much was your meal: ?"))
     percent = percent_to_float(input("How much percent would you like to  tips: ?"))
@@ -5,10 +7,11 @@ def main():
     print(f"Leave ${tips:.2f}")
 
 
-# use string method
+# use string method to remove leading(before string) like $50 ,where $ will be removed !
 def dollars_to_float(d):
     return float(d.strip('$'))
 
+ # use string method to remove trailing(after string) like 50%, % will be removed !
 def percent_to_float(p):
     return float(p.strip('%')) /100
 
