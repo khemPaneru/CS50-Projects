24201ff2472980ae58ff35cdd17055f5d1e99f30
khemPaneru <khempaneru2@gmail.com>
2025-03-23 17:17:41 +0000

    Sun, Mar 23, 2025, 1:17 PM -04:00

--- tip/tip.py
+++ tip/tip.py
@@ -2,5 +2,5 @@ def main():
     dollars = float(input("How much was your meal: ?)")
     percent = float(input("How much percent of meal do you want to give tips: ?)")
     tips = dollars * percent
-    print(f"Leave {tips}.2f)
+    print(f"Leave {tips},.2f)
 main()
