ceb7033366a542eb5eaf3a7f8bb9940b9828d8a4
khemPaneru <khempaneru2@gmail.com>
2025-03-25 16:47:41 +0000

    Tue, Mar 25, 2025, 12:47 PM -04:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,7 @@
 def main():
+
+    # use strip to remove leading or trailing whitespace (spaces, tabs, or newlines)
+
     user_input =input("What is the answer to the Great Question of Life, the Universe and Everything? ").strip().lower()
 
     if user_input=="42" or user_input == "forty-two" or user_input == "forty two":
