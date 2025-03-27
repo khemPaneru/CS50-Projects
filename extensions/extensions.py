b621e9f4ae5f2c0233e44869d464c0a928d65de3
khemPaneru <khempaneru2@gmail.com>
2025-03-27 16:40:20 +0000

    Thu, Mar 27, 2025, 12:40 PM -04:00

---
+++ extensions/extensions.py
@@ -0,0 +1,17 @@
+def main():
+    user_input = input("Enter file name: ").lower()
+
+    media_types = {
+        ".gif": "image/gif",
+        ".jpg": "image/jpg",
+        ".jpeg": "image/jpeg",
+        ".png": "image/png",
+        ".pdf": "application/pdf",
+        ".txt": "text/plain",
+        ".zip": "application/zip",
+    }
+for media in media_types:
+    if user_name.endswitch(media):
+        return media_types[media]
+result = main()
+print(main)
