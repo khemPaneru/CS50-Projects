8a8ba200bed3f222591d9a86224cdc01cdd94670
khemPaneru <khempaneru2@gmail.com>
2025-03-27 17:05:21 +0000

    Thu, Mar 27, 2025, 1:05 PM -04:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,9 +1,7 @@
 def main():
 
     user_input = input("Enter file name: ").lower()
-
     #create a dic
-
     media_types = {
         ".gif": "image/gif",
         ".jpg": "image/jpg",
@@ -13,11 +11,13 @@ def main():
         ".txt": "text/plain",
         ".zip": "application/zip",
     }
+
     for media in media_types:
-        
+#endswith():  method in Python is used to check if a string ends
+# with a specified suffix (or substrings) like .gif,.jpg
         if user_input.endswith(media):
             return media_types[media]
-
+        
     return "application/octet-stream"
 
 print(main())
