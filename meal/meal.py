188b4097851306482aaf0df22da5e8c27079619b
khemPaneru <khempaneru2@gmail.com>
2025-03-29 19:10:58 +0000

    Sat, Mar 29, 2025, 3:10 PM -04:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,9 +1,8 @@
-def main():
 
+def main():
     time= input("What time is it(HH:MM):? ")
 
     time_in_hour = convert(time)
-
 
     if 7.00 <= time_in_hour <= 8.00:
         print("brekfast")
@@ -12,16 +11,13 @@ def main():
     elif 18.00 <= time_in_hour <= 19.0:
         print("Dinner")
 
-
 def convert(clock):
     hours, minutes = clock.split(":")
-
     hours = int(hours)
     minutes = int(minutes)
-
     return hours + (minutes / 60)
 
-
+#use dunder method
 if __name__ == "__main__":
 
     main()
