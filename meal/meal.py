de74cb39b54e361527ca6b02a0eeefc226c0f299
khemPaneru <khempaneru2@gmail.com>
2025-03-29 18:52:03 +0000

    Sat, Mar 29, 2025, 2:52 PM -04:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,23 +1,23 @@
 def main():
 
-    time= input("What time is it(HH:MM) :?")
+    time= input("What time is it(HH:MM):? ")
 
     time_in_hour = convert(time)
 
     if 7.00 <= time_in_hour <= 8.00:
         print("brekfast")
-    elif 13.00 <= time_in_hour <= 14.00:
+    elif 12.00 <= time_in_hour <= 13.00:
         print("lunch")
-    elif 19.00 <= time_in_hour <= 20.0:
+    elif 18.00 <= time_in_hour <= 19.0:
         print("Dinner")
-    else:
-        print("No meal time")
+
 
 def convert(clock):
     hours, minutes = clock.split(":")
 
     hours = int(hours)
     minutes = int(minutes)
+
     return hours + (minutes / 60)
 
 if __name__ == "__main__":
