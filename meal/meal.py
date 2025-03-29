f7d16c7a0d3cff753811f3182c47ced3bbd988da
khemPaneru <khempaneru2@gmail.com>
2025-03-29 18:35:03 +0000

    Sat, Mar 29, 2025, 2:35 PM -04:00

--- meal/meal.py
+++ meal/meal.py
@@ -4,17 +4,17 @@ def main():
 
     time_in_hour = convert(time)
 
-    if 7:00 <= time <= 8:00:
+    if 7:00 <= time_in_hour <= 8:00:
         print("brekfast)"
-    elif 13:00 time <= 14:00:
+    elif 13:00 <= time_in_hour <= 14:00:
         print("lunch")
-    elif 19:00 time <= 20:00:
+    elif 19:00 time_in_hour <= 20:00:
         print("Dinner")
     else:
         print("No meal time")
 
 def convert(clock):
-    hours, minutes = time.split(":")
+    hours, minutes = clock.split(":")
 
     hours = int(hours)
     minutes = int(minutes)
