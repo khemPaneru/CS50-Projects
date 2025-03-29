ddbd9aec5eb5d4a52b491cdeade51029c260aa7f
khemPaneru <khempaneru2@gmail.com>
2025-03-29 07:21:43 +0000

    Sat, Mar 29, 2025, 3:21 AM -04:00

---
+++ interpreter/interpreter.py
@@ -0,0 +1,22 @@
+
+
+expression = input("Expression: ")
+
+x, y, z = expression.split(" ")
+
+x = int(x)
+z = int(z)
+
+if y == "+":
+    result = x + z
+
+elif y == "-":
+    result = x - z
+
+elif y == "*":
+    result = x * z
+
+elif y == "/":
+    result = x / z
+
+print(f"{result:.1f}")
