553f9e83a006e52d5c6f2fb4a322c79578939182
khemPaneru <khempaneru2@gmail.com>
2025-03-29 18:25:58 +0000

    Sat, Mar 29, 2025, 2:25 PM -04:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,14 +1,14 @@
 def main():
 
-time= input("What time is it(HH:MM) :?")
+    time= input("What time is it(HH:MM) :?")
 
-convert(time)
-if 7:00 <= time <= 8:00:
-    print("brekfast)"
-elif 13:00 time <= 14:00:
-    print("lunch")
-elif 19:00 time <= 20:00:
-    print("Dinner")
+    convert(time)
+    if 7:00 <= time <= 8:00:
+        print("brekfast)"
+    elif 13:00 time <= 14:00:
+        print("lunch")
+    elif 19:00 time <= 20:00:
+        print("Dinner")
 
 def convert(clock):
     hours, minutes = time.split(":")
