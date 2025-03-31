e1aa921922250e9dcbd1024b1970d532661479e9
khemPaneru <khempaneru2@gmail.com>
2025-03-31 05:31:35 +0000

    Mon, Mar 31, 2025, 1:31 AM -04:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,6 +3,7 @@ total_amount = 50
 insert_coin = 0
 
 while insert_coin < total_amount:
-    int( input(amount_due:"amount_due =total_amount -insert_coin " ))
-    
+    result = return int( input(amount_due:"amount_due =total_amount -insert_coin " ))
+    print(result)
+
 
