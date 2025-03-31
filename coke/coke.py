75136fa9456032af209c63595f5dc914f989d5ed
khemPaneru <khempaneru2@gmail.com>
2025-03-31 05:39:34 +0000

    Mon, Mar 31, 2025, 1:39 AM -04:00

--- coke/coke.py
+++ coke/coke.py
@@ -5,7 +5,7 @@ insert_coin = 0
 while insert_coin < total_amount:
     amount_due = total_amount - insert_coin
 
-    result = int(input(f"Amount due :{amount_due}"))
+    result = int(input(f"Amount due :{amount_due}. Insert coin"))
     insert_coin += result
     print(f"Total inserted : {insert_coin}")
 
