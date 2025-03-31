4876d363f7ff20ff01045b4642b0de3e0bcfbf1d
khemPaneru <khempaneru2@gmail.com>
2025-03-31 05:52:26 +0000

    Mon, Mar 31, 2025, 1:52 AM -04:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,20 +1,21 @@
+def main():
 
-total_amount = 50
-inserted_coin = 0
+    total_amount = 50
+    inserted_coin = 0
 
-while inserted_coin < total_amount:
-    amount_due = total_amount - inserted_coin
+    while inserted_coin < total_amount:
+        amount_due = total_amount - inserted_coin
 
-    coin = int(input(f"Amount due :{amount_due} cents \nInsert coin"))
-
-
-
-    if coin in [25,10,4]:
-        inserted_coin += coin
-    else:
-        print("invalid")
-change_owe = inserted_coin - total_amount
-if change_owe > 0:
-    print("Change owe{change_owe} cents")
+        coin = int(input(f"Amount due :{amount_due} cents \ nInsert coin"))
 
 
+
+        if coin in [25,10,4]:
+            inserted_coin += coin
+        else:
+            print("invalid")
+    change_owe = inserted_coin - total_amount
+    if change_owe > 0:
+        print("Change owe{change_owe} cents")
+
+main()
