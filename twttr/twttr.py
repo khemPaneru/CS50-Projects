2e4a52d6b8cf333a0e73ed80765b16c0bcd79193
khemPaneru <khempaneru2@gmail.com>
2025-04-01 22:15:42 +0000

    Tue, Apr 1, 2025, 6:15 PM -04:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -3,10 +3,11 @@ user_input = input("Input: ")
 
 vowel = "aeiouAEIOU"
 
+print("Output:", end=" ")
 
 for char in user_input:
     if char not in  vowel:
         #It's an argument or parameter passed to the print() function used to
         #  peint in same line without introducing
         #  new line for printing as python does by default
-        print('Output:', char, end="")
+        print( char, end="")
