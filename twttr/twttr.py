9a276e587a5d646a96c2347b2072285b87dc93a5
khemPaneru <khempaneru2@gmail.com>
2025-04-01 21:47:57 +0000

    Tue, Apr 1, 2025, 5:47 PM -04:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,10 +1,10 @@
 
 user_input = input("Input")
 
-vowel = {a,e,i,o,u}
+vowel = "aeiouAEIOU"
 result =""
 
 for char in user_input
-    if char !== vowel:
+    if char != vowel:
         result += char
 print(f{result})
