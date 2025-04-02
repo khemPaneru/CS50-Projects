447e4289a88ed07d6609f55f26acc63bbe0e7fb0
khemPaneru <khempaneru2@gmail.com>
2025-04-02 22:01:47 +0000

    Wed, Apr 2, 2025, 6:01 PM -04:00

--- plates/plates.py
+++ plates/plates.py
@@ -15,11 +15,11 @@ if not (2< lens(khem) >= 6):
 if not khem[:2]   #or khem[0:2]
     return false
 
-for i in range(lens(khem)):
-   if khem[i].isdigits():
+for i in range(len(khem)):
+   if khem[i].isdigit():
         if khem[i] == 0:
             return false
-        if not khem[i].isdidgits():
+        if not khem[i].isdidgit():
             return false
         break
 
