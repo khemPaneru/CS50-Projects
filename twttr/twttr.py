7ca28ca3d60932adb26f6d940db355d356d8d835
khemPaneru <khempaneru2@gmail.com>
2025-04-02 20:51:29 +0000

    Wed, Apr 2, 2025, 4:51 PM -04:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -9,6 +9,8 @@ print("Output:", end=" ")
 
 for char in user_input:
     if char not in  vowel:
+        #end="" :parameter is used in the print() function to specify what should be printed at the end
+        # of the output instead of the default newline (\n).
 
         print( char, end="")
 print()
