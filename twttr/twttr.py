da5b4aec8525d032999acbb61d8d54243d22ed58
khemPaneru <khempaneru2@gmail.com>
2025-04-02 20:50:10 +0000

    Wed, Apr 2, 2025, 4:50 PM -04:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,7 +1,7 @@
 
-# end="" :It's an argument or parameter passed to the print() function used to
-        #  peint in same line without introducing
-        #  new line for printing as python does by defaultuser_input = input("Input: ")
+
+
+user_input = input("Input: ")
 
 vowel = "aeiouAEIOU"
 
