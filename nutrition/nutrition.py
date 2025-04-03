00c0f45bd524ba392144d5581d64b298ab38ab9e
khemPaneru <khempaneru2@gmail.com>
2025-04-03 04:35:15 +0000

    Thu, Apr 3, 2025, 12:35 AM -04:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,11 +1,10 @@
 
+
+def  calories_in_calories(s):
+
+    if Apple == 18:
+        return True
+
 user_input = input("Item: ")
 khem = calories_in_calories(user_input)
 print(khem)
-
-def  calories_in_calories(s):
-fruits_dic =
-{
-"Apple":
-
-}
