f370d629e3431d8219de7fed06447e3cb8d36236
khemPaneru <khempaneru2@gmail.com>
2025-04-04 00:34:10 +0000

    Thu, Apr 3, 2025, 8:34 PM -04:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -3,30 +3,28 @@
 def main():
     fruits = {
         "apple": 130,
-        "Avocado": 50,
-        "Banana": 110,
-        "Cantaloupe": 50,
-        "Grapefruit": 60,
-        "Grapes": 90,
-        "Honeydew Melon": 50,
-        "Kiwifruit": 90,
-        "Lemon": 15,
+        "avocado": 50,
+        "banana": 110,
+        "cantaloupe": 50,
+        "grapefruit": 60,
+        "grapes": 90,
+        "honeydew melon": 50,
+        "kiwifruit": 90,
+        "lemon": 15,
         "Lime": 20,
-        "Nectarine": 60,
-        "Orange": 80,
-        "Peach": 60,
-        "Pear": 100,
-        "Pineapple": 50,
-        "Plums": 70,
-        "Strawberries": 50,
-        "Sweet Cherries": 100,
-        "Tangerine": 50,
-        "Watermelon": 80
+        "nectarine": 60,
+        "orange": 80,
+        "peach": 60,
+        "pear": 100,
+        "pineapple": 50,
+        "plums": 70,
+        "strawberries": 50,
+        "sweet cherries": 100,
+        "tangerine": 50,
+        "watermelon": 80
             }
 
-    fruit = input("Item: ").lower(
-        
-    )
+    fruit = input("Item: ").lower()
     if fruit in fruits:
         print(f"Calories: {fruits[fruit]}")
 
