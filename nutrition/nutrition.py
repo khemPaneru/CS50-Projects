be96e1408f130c7506f373fde85a871f3dbd3534
khemPaneru <khempaneru2@gmail.com>
2025-04-04 00:19:42 +0000

    Thu, Apr 3, 2025, 8:19 PM -04:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,30 +1,33 @@
 
 
+def main():
+    fruits = {
+        "Apple": 130,
+        "Avocado": 50,
+        "Banana": 110,
+        "Cantaloupe": 50,
+        "Grapefruit": 60,
+        "Grapes": 90,
+        "Honeydew Melon": 50,
+        "Kiwifruit": 90,
+        "Lemon": 15,
+        "Lime": 20,
+        "Nectarine": 60,
+        "Orange": 80,
+        "Peach": 60,
+        "Pear": 100,
+        "Pineapple": 50,
+        "Plums": 70,
+        "Strawberries": 50,
+        "Sweet Cherries": 100,
+        "Tangerine": 50,
+        "Watermelon": 80
+            }
 
-fruits = {
-    "Apple": 130,
-    "Avocado": 50,
-    "Banana": 110,
-    "Cantaloupe": 50,
-    "Grapefruit": 60,
-    "Grapes": 90,
-    "Honeydew Melon": 50,
-    "Kiwifruit": 90,
-    "Lemon": 15,
-    "Lime": 20,
-    "Nectarine": 60,
-    "Orange": 80,
-    "Peach": 60,
-    "Pear": 100,
-    "Pineapple": 50,
-    "Plums": 70,
-    "Strawberries": 50,
-    "Sweet Cherries": 100,
-    "Tangerine": 50,
-    "Watermelon": 80
-        }
+    fruit = input("Item: ")
+    if fruit in fruits:
+        print(f"Calories: {fruits[fruit]}")
 
-fruit = input("Item: ").lower()
-if fruit in fruits:
-    print(f"Calories: {fruits[fruit]}")
+if __name__ == "__main__":
+    main()
 
