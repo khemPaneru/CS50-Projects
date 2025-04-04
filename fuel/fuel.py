44e9049f58f747352a26070c2b3adf12e74f6394
khemPaneru <khempaneru2@gmail.com>
2025-04-04 23:30:08 +0000

    Fri, Apr 4, 2025, 7:30 PM -04:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,11 +1,8 @@
 def main():
     while True:
         try:
-
             user_input = input("Fraction: ")
-
             x, y = user_input.split('/')
-
             x = int(x)
             y = int(y)
 
@@ -13,16 +10,13 @@ def main():
                 raise ValueError
             if y == 0:
                raise ZeroDivisionError
-
             break
         except ValueError:
             print("parts can't be greater than whole" )
 
         except ZeroDivisionError:
             print("Total can't be zero")
-
     percentage = (x / y) * 100
-
     if percentage <= 1:
         print("E")
     elif percentage >= 99:
