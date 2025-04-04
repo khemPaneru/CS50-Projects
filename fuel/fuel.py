05a023a28fa49dff8535a47e4a380a69a97ff24e
khemPaneru <khempaneru2@gmail.com>
2025-04-04 22:44:44 +0000

    Fri, Apr 4, 2025, 6:44 PM -04:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,12 +1,30 @@
+def main():
+    while True:
+        try:
 
+            user_input = input("Fraction: ")
 
+            x, y = user_input.split('/')
 
-def fuel_gauge(para):
-try: 
+            x = int(x)
+            y = int(y)
 
+            if x > y:
+                print("Error: Num can't be greater than deno")
+                continue
+            if y == 0:
+                print("Error: Denominator can't be 0.")
+                continue
+            break
+        except (ValueError, ZeroDivisionError):
+            print("Invalid input. Please enter a valid fraction in the form x/y.")
 
+    percentage = (x / y) * 100
 
-user_input = input("Fraction: x/y").split("/")
-fuel_gauge(user_input)
-try:
-
+    if percentage <= 1:
+        print("E")
+    elif percentage >= 99:
+        print("F")
+    else:
+        print(f"{round(percentage)}%")
+main()
