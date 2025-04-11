33d5cbfe53d5f2106b7786ee9093460290ebb8fa
khemPaneru <khempaneru2@gmail.com>
2025-04-11 18:21:35 +0000

    Fri, Apr 11, 2025, 2:21 PM -04:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -13,11 +13,11 @@ def main():
         except EOFError:
             break
 
-    # sorted() helps to Sort dictionary keys alphabetically
+    # sorted() = to Sort dictionary keys alphabetically
     sorted_items = sorted(grocery_list.keys())
 
     for item in sorted_items:
         print(f"{grocery_list[item]} {item.upper()}")
-
+        
 if __name__ == "__main__":
     main()
