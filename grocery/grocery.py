6af4857ad70bf66a6d2c22efc7eeb2bb6b9c5cc4
khemPaneru <khempaneru2@gmail.com>
2025-04-11 18:02:18 +0000

    Fri, Apr 11, 2025, 2:02 PM -04:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,19 +1,25 @@
 def main():
     grocery_list = {}
+
     while True:
         try:
-            item = input("input").strip()
-            if item:  # If the user input is not empty
-                item = item.lower()
+            item = input().strip()
+            if item:  # If the input is not empty
+                item = item.lower()  # Convert input to lowercase
                 if item in grocery_list:
-                    grocery_list[item] += 1
+                    grocery_list[item] += 1  # Increase count
                 else:
-                    grocery_list[item] = 1
+                    grocery_list[item] = 1  # Initialize count
         except EOFError:
-            break
+            break  # Exit the loop when EOF is encountered
 
+    # Sort dictionary keys alphabetically
     sorted_items = sorted(grocery_list.keys())
 
+    # Print grocery list
     for item in sorted_items:
         print(f"{grocery_list[item]} {item.upper()}")
 
+# Run the main function
+if __name__ == "__main__":
+    main()
