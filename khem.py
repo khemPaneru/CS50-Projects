d9e603b3bee13cbbd38403e55a1539a48eac2073
khemPaneru <khempaneru2@gmail.com>
2025-04-12 16:40:18 +0000

    Sat, Apr 12, 2025, 12:40 PM -04:00

---
+++ khem.py
@@ -0,0 +1,7 @@
+months_name = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]
+khem = months_name.index("November") +1
+
+print(khem)
