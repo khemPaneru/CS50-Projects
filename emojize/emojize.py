57a190ba19fb2e0993b89b3d8fad2b818bdf766f
khemPaneru <khempaneru2@gmail.com>
2025-04-14 05:25:23 +0000

    Mon, Apr 14, 2025, 1:25 AM -04:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -4,6 +4,6 @@ import emoji
 def main():
     text = input("Input ")
     converted_text = emoji.emojize(text, language="alias")
-    print(f"Output: ",     converted_text)
+    print(f"Output: ",  converted_text)
 if __name__ =="__main__":
     main()
