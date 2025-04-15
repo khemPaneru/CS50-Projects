3f08527c343d80a3b0879b712aaa76f2fd20d7b9
khemPaneru <khempaneru2@gmail.com>
2025-04-15 16:51:16 +0000

    Tue, Apr 15, 2025, 12:51 PM -04:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,2 +1,21 @@
+import sys
+import random
 from pyfiglet import Figlet
+
+fig = figlet()
+fonts_available = figlet.getfonts()
+if len(sys.argv) = 1:
+font = random.choice(fonts_available)
+elif len(sys.argv) ==3 and (sys.argv[1] == "-f" or sys.arv[1] == "__font"):
+
+    if sys.argv[2] in fonts_available:
+        font = sys.argv[2]
+    else:
+        sys.exit("invalid")
+else:
+sys.exit("exit")
+fif.setFont(font=font)
+text = input("Input: ")
+print(fig.RenderText(text))
+
 
