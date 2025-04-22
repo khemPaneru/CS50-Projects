869c5c1f7fa2f5c9954e3c3c3d017a2c72abcf2c
khemPaneru <khempaneru2@gmail.com>
2025-04-22 05:03:31 +0000

    Tue, Apr 22, 2025, 1:03 AM -04:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -5,4 +5,7 @@ if len(sys.argv) !=2:
     sys.exit(""Missing command-line argument"")
 
 try:
-    bitcoin = input()
+    bitcoin = float(sys.argv[1])
+
+except ValueError:
+    sys.exit("Command-line argument is not a number")
