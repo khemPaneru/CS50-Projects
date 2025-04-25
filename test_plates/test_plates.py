6a16e2b5a51a6d053e9dcb0129c91fa93796815f
khemPaneru <khempaneru2@gmail.com>
2025-04-25 22:20:39 +0000

    Fri, Apr 25, 2025, 6:20 PM -04:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,4 +1,22 @@
-from plates imort is_valid
 
-def :
-    assert
+from plates import is_valid
+
+def test_length():
+    assert is_valid("CS50") == True
+    assert is_valid("C") == False
+    assert is_valid("CS50PY") == True
+    assert is_valid("CS50python") == False
+
+def test_start_letters():
+    assert is_valid("CS50") == True
+    assert is_valid("50CS") == False
+
+def test_numbers_end():
+    assert is_valid("Cs50") == True
+    assert is_valid("CS05") == False
+    assert is_valid("CS5O0") == False
+
+def test_no_special_characters():
+    assert is_valid("CS!50") == False
+    assert is_valid("CS 50") == False
+    assert is_valid("CS-50") == False
