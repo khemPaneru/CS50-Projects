b9156b3ad5cd4f4d9bc50c060a776fdba3093993
khemPaneru <khempaneru2@gmail.com>
2025-04-25 22:57:40 +0000

    Fri, Apr 25, 2025, 6:57 PM -04:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -9,6 +9,11 @@ def test_length():
 def test_start_letters():
     assert is_valid("CS50") == True
     assert is_valid("50CS") == False
+    assert is_valid("5C50") == False
+    assert is_valid("1ABC") == False
+    assert is_valid("C5") == False
+    assert is_valid("9Z9Z") == False
+
 
 def test_numbers_end():
     assert is_valid("Cs50") == True
