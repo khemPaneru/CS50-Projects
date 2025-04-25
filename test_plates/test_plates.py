cf0bc8369ceb34cfc6c7515d1b2d871fcd352cf1
khemPaneru <khempaneru2@gmail.com>
2025-04-25 22:58:47 +0000

    Fri, Apr 25, 2025, 6:58 PM -04:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,26 +1,27 @@
-
 from plates import is_valid
 
-def test_length():
+def test_valid():
     assert is_valid("CS50") == True
-    assert is_valid("C") == False
-    assert is_valid("CS50python") == False
+    assert is_valid("HELLO") == True
+    assert is_valid("OUTATIME") == False  # too long
 
-def test_start_letters():
+def test_start_with_two_letters():
     assert is_valid("CS50") == True
+    assert is_valid("C50") == False
     assert is_valid("50CS") == False
-    assert is_valid("5C50") == False
-    assert is_valid("1ABC") == False
-    assert is_valid("C5") == False
-    assert is_valid("9Z9Z") == False
 
+def test_length():
+    assert is_valid("A") == False
+    assert is_valid("ABCDEFG") == False
+    assert is_valid("AB") == True
+    assert is_valid("ABCDEF") == True
 
-def test_numbers_end():
-    assert is_valid("Cs50") == True
+def test_number_rules():
+    assert is_valid("CS50") == True
     assert is_valid("CS05") == False
-    assert is_valid("CS5O0") == False
+    assert is_valid("CS50A") == False
 
-def test_no_special_characters():
+def test_alphanumeric():
     assert is_valid("CS!50") == False
     assert is_valid("CS 50") == False
     assert is_valid("CS-50") == False
