ccb5e8fb1fca4993c8e875dfb7eb7183885c995f
khemPaneru <khempaneru2@gmail.com>
2025-04-26 16:48:02 +0000

    Sat, Apr 26, 2025, 12:48 PM -04:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,2 +1,15 @@
 
 from fuel import convert, gauge
+
+def test_convert_valid():
+    assert convert("3/4") == 75
+    assert convert("1/4") == 25
+    assert convert("1/1") == 100
+    assert convert("0/1") == 0
+    
+def test_gauge():
+    assert gauge(1) == "E"
+    assert gauge(0) == "E"
+    assert gauge(99) == "F"
+    assert gauge(100) == "F"
+    assert gauge(50) == "50%"
