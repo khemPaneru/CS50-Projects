1f38d51a517bad97bde5b5dc642076b67e341050
khemPaneru <khempaneru2@gmail.com>
2025-04-26 15:59:31 +0000

    Sat, Apr 26, 2025, 11:59 AM -04:00

---
+++ test_fuel/fuel.py
@@ -0,0 +1,29 @@
+
+#copy one
+def main():
+    while True:
+        try:
+            user_input = input("Fraction: ")
+            x, y = user_input.split('/')
+            x = int(x)
+            y = int(y)
+
+            if x > y:
+                raise ValueError
+            if y == 0:
+               raise ZeroDivisionError
+            break
+        except ValueError:
+            print("Fractional parts can't be greater than whole" ) #num
+
+        except ZeroDivisionError:
+            print("Deno(whole) can't be zero")
+    percentage = (x / y) * 100
+    if percentage <= 1:
+        print("E")
+    elif percentage >= 99:
+        print("F")
+    else:
+        print(f"{round(percentage)}%")
+main()
+
