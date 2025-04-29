f321fe988288e93f97e498435b077d8f8b34dafb
khemPaneru <khempaneru2@gmail.com>
2025-04-29 17:41:25 +0000

    Tue, Apr 29, 2025, 1:41 PM -04:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -3,12 +3,15 @@
 import sys
 import csv
 
-if len(sys.argv) =! 2:
+if len(sys.argv) != 2:
     sys.exit("Too few command-line arguments")
+
+if len(sys.argv) < 2:
+    sys.exit("Too many command-line arguments")
 
 filename = sys.argv[1]
 
-if not filename endwith(".csv"):
+if not filename.endswith(".csv"):
         sys.exit("Not a CSV file")
 
 try:
