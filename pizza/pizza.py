07923818f3ec4f609d71576d4a361dbbf0824364
khemPaneru <khempaneru2@gmail.com>
2025-04-29 17:07:39 +0000

    Tue, Apr 29, 2025, 1:07 PM -04:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,6 +1,16 @@
 
-if len(sys.argv) =! 2
-    sys.exit()
 
-if not filename endwith(.csv)
-    sys.exit()
+import sys
+import csv
+
+if len(sys.argv) =! 2
+    sys.exit("Too few command-line arguments")
+
+filename = sys.argv[1]
+
+if not filename endwith(".csv"):
+        sys.exit("Not a CSV file")
+
+try:
+      with open(filename) as file:
+            reader = csv.reader(file)
