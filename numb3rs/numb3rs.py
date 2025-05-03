5706df5269762e1df7f1685ff8a45a17b45541c8
khemPaneru <khempaneru2@gmail.com>
2025-05-03 15:40:32 +0000

    Sat, May 3, 2025, 11:40 AM -04:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -1,7 +1,8 @@
 import re
 
 def main():
-    prit
+    user_input = input("IPV4 Address: ")
+    print(validate)
 
 def validate(ip):
     if not re.match(r"^\d+\.\d+\.\d+\.\d+$", ip):
@@ -10,3 +11,12 @@ def validate(ip):
         octets = ip.split(".")
         if len(octets) !=4:
             return False
+        for octet in octets:
+            if not 0 <= int(octet) <= 255:
+                return False
+
+         return True
+    except ValueError:
+        return False
+if __name__ == "__main__"
+    main()
