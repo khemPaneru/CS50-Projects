f11730f63fe678769e9e4fc9bc95cc94d5f861cd
khemPaneru <khempaneru2@gmail.com>
2025-05-09 03:47:55 +0000

    Thu, May 8, 2025, 11:47 PM -04:00

---
+++ watch/watch.py
@@ -0,0 +1,18 @@
+
+
+import re
+import sys
+
+def main():
+    print(parse(input("HTML: ")))
+
+def parse(s):
+    
+    match = re.search(r'<iframe[^>]*src="https?://(?:www\.)?youtube\.com/embed/([a-zA-Z0-9_-]+)"', s)
+    if match:
+        video_id = match.group(1)
+        return f"https://youtu.be/{video_id}"
+    return None
+
+if __name__ == "__main__":
+    main()
