8833bc7429513584acef1aa6bf6f7cd91f4c141e
khemPaneru <khempaneru2@gmail.com>
2025-05-10 01:29:20 +0000

    Fri, May 9, 2025, 9:29 PM -04:00

--- working/test_working.py
+++ working/test_working.py
@@ -6,3 +6,7 @@ def test_valid_input():
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
     assert convert("10 AM to 8:50 PM") == "10:00 to 20:50"
     assert convert("10:30 PM to 8 AM") == "22:30 to 08:00"
+
+def test_invalid_input():
+    with pytest.raises(ValueError):
+        
