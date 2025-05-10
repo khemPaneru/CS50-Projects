2910b68168e074b206c15435a37f2ac8c1ab3bc5
khemPaneru <khempaneru2@gmail.com>
2025-05-10 01:40:51 +0000

    Fri, May 9, 2025, 9:40 PM -04:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,22 +1,25 @@
 import pytest
 from working import convert
 
-def test_valid_input():
+def test_valid_times():
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
-    assert convert("10 AM to 8:50 PM") == "10:00 to 20:50"
+    assert convert("10:30 AM to 8:50 PM") == "10:30 to 20:50"
     assert convert("10:30 PM to 8 AM") == "22:30 to 08:00"
 
-def test_invalid_input():
+def test_invalid_times():
     with pytest.raises(ValueError):
-     convert("12:60 AM to 5:60 PM")
+        convert("9:60 AM to 5:60 PM")
 
-      with pytest.raises(ValueError):
+    with pytest.raises(ValueError):
         convert("9 AM - 5 PM")
 
-        with pytest.raise(ValueError):
-            convert("09:00 AM - 17:00 PM")
-            
+    with pytest.raises(ValueError):
+        convert("12:00 PM to 13:00 AM")
+
+    with pytest.raises(ValueError):
+        convert("09:00 AM - 17:00 PM")
+
 def test_edge_cases():
     assert convert("12:00 AM to 12:00 PM") == "00:00 to 12:00"
     assert convert("12:00 PM to 12:00 AM") == "12:00 to 00:00"
