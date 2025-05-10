cce41e3e48a2ea40dacc571c23238668b6a90bcd
khemPaneru <khempaneru2@gmail.com>
2025-05-10 00:21:27 +0000

    Fri, May 9, 2025, 8:21 PM -04:00

--- working/working.py
+++ working/working.py
@@ -8,7 +8,11 @@ def main():
 
 
 def convert(s):
-
+    pattern = r"^(\d{1,2})(?::(\d{2}))? (AM|PM) to (\d{1,2})(?::(\d{2}))? (AM|PM)$
+    match = re.match(pattern, s)
+if not match:
+    raise ValueError("Invalid input format")
+h1, m1, am_pm1, h2, m2, am_pm2 = match.group()
 
 if __name__ == "__main__":
     main()
