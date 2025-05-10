43b54412a1a7f8236357032428bf2dcad3c21c4f
khemPaneru <khempaneru2@gmail.com>
2025-05-10 00:26:06 +0000

    Fri, May 9, 2025, 8:26 PM -04:00

--- working/working.py
+++ working/working.py
@@ -12,7 +12,14 @@ def convert(s):
     match = re.match(pattern, s)
 if not match:
     raise ValueError("Invalid input format")
-h1, m1, am_pm1, h2, m2, am_pm2 = match.group()
+h1, m1, am_pm1, h2, m2, am_pm2 = match.group(1), match.group(2), match.group(3), match.group(4), match.group(5), match.group(6)
 
+m1 = m1 if m1 else "00"
+m2 = m2 if m2 else "00"
+
+h1, m1 = int(h1), int(m1)
+h2, m2 = int(h2), int(m2)
+
+if not (0<= m1 )
 if __name__ == "__main__":
     main()
