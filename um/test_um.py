aeae91af40c8ca5e524e5bd46c9795cf1bf9bbc0
khemPaneru <khempaneru2@gmail.com>
2025-05-11 13:52:01 +0000

    Sun, May 11, 2025, 9:52 AM -04:00

--- um/test_um.py
+++ um/test_um.py
@@ -5,12 +5,16 @@ def test_count_1():
     assert count("um") == 1
 
 def test_count_2():
-    assert count("umm, Hello") == 1
+    assert count("Um") == 1
 
 def test_count_3():
+    assert count("Hello, um, world.") == 1
+
+def test_count_4():
     assert count("yummy") == 0
 
-def test_count_(4):
-    assert count("um?") == 1
 def test_count_5():
     assert count("Um, thanks, um...") == 2
+
+def test_count_6():
+    assert count("um?") == 1
