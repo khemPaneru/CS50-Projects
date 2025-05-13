13bf849288f3053d3db86007e194652b1ca552b0
khemPaneru <khempaneru2@gmail.com>
2025-05-13 18:13:23 +0000

    Tue, May 13, 2025, 2:13 PM -04:00

---
+++ seasons/test_seasons.py
@@ -0,0 +1,17 @@
+
+from seasons import calculate_minutes , convert_to_words
+from datetime import date, timedelta
+
+def test_calculate_minutes():
+    today = date.today()
+    one_day_ago = today - timedelta(days=1)
+    assert calculate_minutes(one_day_ago) == 1440 # 1 day = 1440 minutes
+
+
+    two_days_ago = today - timedelta(days=2)
+    assert calculate_minutes(two_days_ago) == 2880
+
+def test_convert_to_words():
+    assert convert_to_words(1440) == "one thousand four hundred forty"
+    assert convert_to_words(525600) == "five hundred twenty-five thousand six hundred"
+    assert convert_to_words(1051200) == "one million fifty-one thousand two hundred"
