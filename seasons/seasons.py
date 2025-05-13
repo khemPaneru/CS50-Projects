fcd1ebdd1adbc9597a67ca8e95817776f71a3fee
khemPaneru <khempaneru2@gmail.com>
2025-05-13 16:33:11 +0000

    Tue, May 13, 2025, 12:33 PM -04:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -5,3 +5,12 @@ import sys
 
 def main():
     birth_str = input("Date of Birth(YYYY-MM-DD: "))
+
+    try:
+        birth_date = parse_date(birth_str):
+    except ValueError:
+        sys.exit("Invalid date")
+
+def parse_date(date_str):
+    try:
+        
